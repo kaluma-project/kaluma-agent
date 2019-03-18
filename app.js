24e6fc24b2f8f139904c6d77d0b4ed2924d760ee
niklauslee <niklaus.lee@gmail.com>
2019-03-18 15:12:59 +0900

    electronized

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const config = require('./config')
 // const path = require('path')
+const electron = require('electron')
+const electronApp = electron.app
+const {Menu, Tray} = electron
 
 const SerialManager = require('./lib/serial-manager')
 
 let serialManager = null
-
-/*
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
-*/
+let tray = null
+let contextMenu = null
 
 io.on('connection', function (socket) {
   console.log('socket.io connected')
@@ -25,6 +24,49 @@ io.on('connection', function (socket) {
   })
 })
 
-http.listen(4000, function () {
-  console.log('listening on *:4000')
+function updateMenu () {
+  var startMenu = contextMenu.getMenuItemById('start')
+  var stopMenu = contextMenu.getMenuItemById('stop')
+  if (http.listening) {
+    startMenu.enabled = false
+    stopMenu.enabled = true
+  } else {
+    startMenu.enabled = true
+    stopMenu.enabled = false
+  }
+}
+
+function startAgent () {
+  if (!http.listening) {
+    http.listen(4000, function () {
+      console.log('listening on *:4000')
+      updateMenu()
+    })
+  }
+}
+
+function stopAgent () {
+  if (http.listening) {
+    http.close(function () {
+      console.log('stop listening')
+      updateMenu()
+    })
+  }
+}
+
+function closeAgent () {
+  electronApp.quit()
+}
+
+electronApp.on('ready', () => {
+  tray = new Tray('res/images/kameleonTemplate.png')
+  contextMenu = Menu.buildFromTemplate([
+    { id: 'start', label: 'Start Agent', click: startAgent },
+    { id: 'stop', label: 'Stop Agent', click: stopAgent },
+    { type: 'separator' },
+    { id: 'close', label: 'Close', click: closeAgent }
+  ])
+  tray.setToolTip('Kameleon Agent')
+  tray.setContextMenu(contextMenu)
+  startAgent()
 })
